15fd770c61366502bce0e5e70a2de52c3bafdc96
saboshi69 <42663794+saboshi69@users.noreply.github.com>
2019-01-24 00:29:33 +0800

    stable v1.0

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const socket = require('socket.io');
+const app = express()
+let server = app.listen(8000, () => { console.log('listen port 8000') })
+let io = socket(server)
+
+
+app.use(express.static('public'))
+
+
+let qnaarr = ["Hello, please enter the number of the question (1-5). ", "1. Can I make a reservation?", "2. Do you deliver / do carry-outs?", "3. Opening & Closing Hour", "4. Do you have a vegetarian / gluten free option?", "5. Where's the location?"]
+
+io.on('connection', function (socket) {
+
+    socket.on('subscribe', function (room) {
+        console.log('joining room', room);
+        socket.join(room);
+
+        for (question of qnaarr) {
+
+            io.sockets.in(room).emit('pm', {
+                handle: "Annie",
+                message: question
+            })
+        }
+    });
+
+
+
+    socket.on('chat', function (data) {   //actions when receieve command (on) from socket
+        console.log('getting incomming chat from room ', data.room)
+        console.log('User request: ', data.message)
+        switch (data.message) {
+            case '1':
+                io.sockets.in(data.room).emit('pm', {
+                    handle: "Annie",
+                    message: `Unfortunately, we do not accept reservations.   We offer first-come, first-serve.`
+
+                })
+                break;
+            case '2':
+                io.sockets.in(data.room).emit('pm', {
+                    handle: "Annie",
+                    message: `We do for lunch services.  We are dine-in only during dinner hours.  We want you to eat ramen noodles al dente and broth steaming hot.   For that reason, we recommend dine-in.  `
+
+                })
+                break;
+            case '3':
+                io.sockets.in(data.room).emit('pm', {
+                    handle: "Annie",
+                    message: `Mon - Sun: 11:30 - 22:30`
+
+                })
+                break;
+            case '4':
+                io.sockets.in(data.room).emit('pm', {
+                    handle: "Annie",
+                    message: `Yes we do.   Please ask our servers a few options.`
+
+                })
+                break;
+            case '5':
+                io.sockets.in(data.room).emit('pm', {
+                    handle: "Annie",
+                    message: `Shop G814, Yuen Kung Mansion,, 108 Taikoo Shing Road, Taikoo Shing`
+
+                })
+                break;
+            default:
+                io.sockets.in(data.room).emit('pm', {
+                    handle: "Annie",
+                    message: `Sorry I dont understand, please enter the Number of the question :(`
+
+                })
+        }
+
+    })
+
+})
---
+++ public/assets/chat.js
@@ -0,0 +1,66 @@
+//front end connection
+
+
+let socket = io.connect('http://localhost:8000')
+let message = document.querySelector("#message")
+let btn = document.querySelector("#send")
+let handle = document.querySelector("#handle")
+let output = document.querySelector("#output")
+let chatwindow = document.querySelector("#chat-window")
+
+
+
+let conversation_id = "" + Math.random()
+socket.emit('subscribe', conversation_id);
+
+
+//submit events to io 
+btn.addEventListener('click', function () {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value,
+        room: conversation_id
+    })
+
+    output.innerHTML += '<p><strong>' + handle.value + ': </strong>' + message.value + '</p>'
+    chatwindow.scrollTop = chatwindow.scrollHeight
+    message.value = ''
+})
+
+message.addEventListener('keypress', function (event) {
+    if (event.keyCode == 13) {
+        socket.emit('chat', {
+            message: message.value,
+            handle: handle.value,
+            room: conversation_id
+        })
+
+        output.innerHTML += '<p><strong>' + handle.value + ': </strong>' + message.value + '</p>'
+        chatwindow.scrollTop = chatwindow.scrollHeight
+        message.value = ''
+    }
+})
+
+
+// message.addEventListener('keypress', function(){
+//     if (event.keyCode !== 13){
+//         socket.emit('isTyping', handle.value)
+//     }
+// })
+
+
+
+//listen events from io
+socket.on('pm', function (data) {
+
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+    chatwindow.scrollTop = chatwindow.scrollHeight
+})
+
+
+
+// socket.on('isTyping', function(data){
+//     console.log('hello')
+//     echo.innerHTML = '<p><em>'+ data +' is typing a message... </em></p>'
+// })
+
